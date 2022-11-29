ff6df3edb0e0e421bf190df8931cc968a8d71295
dlion168 <even.dlion8@gmail.com>
2022-11-29 08:08:37 +0800

    only commit

---
+++ hack2/backend/src/routes/info.js
@@ -0,0 +1,87 @@
+// * ////////////////////////////////////////////////////////////////////////
+// *
+// * FileName     [ info.js ]
+// * PackageName  [ server ]
+// * Synopsis     [ Get restaurant info from database ]
+// * Author       [ Chin-Yi Cheng ]
+// * Copyright    [ 2022 11 ]
+// *
+// * ////////////////////////////////////////////////////////////////////////
+
+import Info from '../models/info'
+
+exports.GetSearch = async (req, res) => {
+    console.log(req.query)
+    /*******    NOTE: DO NOT MODIFY   *******/
+    const priceFilter = req.query.priceFilter
+    const mealFilter  = req.query.mealFilter
+    const typeFilter  = req.query.typeFilter
+    const sortBy      = req.query.sortBy
+    /****************************************/
+    const condition ={}
+    console.log(sortBy)
+    // // NOTE Hint: 
+    await Info.find({condition}).exec((err, data)=>{
+        console.log(err)
+        if (err){
+            return res.status(403).send({ message: 'error', contents: err })
+            }
+        else {
+            let index = data.length -1
+            if (priceFilter || mealFilter || typeFilter){
+                while (index >=0) {
+                    if (!(data[index].price in priceFilter) || !(data[index].meal in mealFilter) || !(data[index].meal in typeFilter)){
+                        data.splice(index,1)
+                    }
+                    index -= 1
+                }
+            }
+
+            if (sortBy == 'price'){
+                console.log(data)
+                data = data.sort((p1,p2) => (p1.price < p2.price))
+                
+            }
+            else if (sortBy == 'distance'){
+                data = data.sort((p1,p2) => (p1.distance <p2.distance))
+            }
+            return res.status(200).send({ message: 'success', contents: data })
+        }
+        }
+    )
+
+    // TODO Part I-3-a: find the information to all restaurants
+    
+    // TODO Part II-2-a: revise the route so that the result is filtered with priceFilter, mealFilter and typeFilter
+    // TODO Part II-2-b: revise the route so that the result is sorted by sortBy
+
+}
+
+exports.GetInfo = async (req, res) => {
+    /*******    NOTE: DO NOT MODIFY   *******/
+    const id = req.query.id
+    /****************************************/
+
+    // NOTE USE THE FOLLOWING FORMAT. Send type should be 
+    // if success:
+    // {
+    //    message: 'success'
+    //    contents: the data to be sent. Hint: A dictionary of the restaruant's information.
+    // }
+    // else:
+    // {
+    //    message: 'error'
+    //    contents: []
+    // }
+
+    // TODO Part III-2: find the information to the restaurant with the id that the user requests
+    const condition = {id: id}
+    await Info.collection.find({condition}).exec((err, data)=>{
+        console.log(err, data)
+        if (err){
+            return res.status(200).send({ message: 'error', contents: [] })}
+        else {
+            return res.status(403).send({ message: 'success', contents: res })}
+        }
+    )
+}
---
+++ hack2/frontend/src/container/searchPage.js
@@ -0,0 +1,68 @@
+/****************************************************************************
+  FileName      [ searchPage.js ]
+  PackageName   [ src ]
+  Author        [ Chin-Yi Cheng ]
+  Synopsis      [ display the search result ]
+  Copyright     [ 2022 11 ]
+****************************************************************************/
+
+import React, { useState, useEffect } from 'react'
+import '../css/searchPage.css'
+import { useNavigate, useLocation } from 'react-router-dom'
+
+import axios from 'axios'
+const instance = axios.create({
+    baseURL: 'http://localhost:4000/api'
+})
+
+const SearchPage = () => {
+    const { state } = useLocation();
+    const [restaurants, setRestaurant] = useState([])
+    const getRestaurant = async () => {
+        console.log(state)
+        const data = await instance.get('/getSearch', { params: state  });
+        console.log(data)
+        if (data.data.message === 'success') setRestaurant(data.data.contents)
+    }
+
+    useEffect(() => {
+        getRestaurant()
+    }, [state.priceFilter, state.mealFilter, state.typeFilter, state.sortBy])
+
+
+    const navigate = useNavigate();
+    const ToRestaurant = (id) => {
+        navigate(`/restaurant/${id}`);
+        // TODO Part III-1: navigate the user to restaurant page with the corresponding id
+    }
+    const getPrice = (price) => {
+        let priceText = ""
+        for (let i = 0; i < price; i++)
+            priceText += "$"
+        return (priceText)
+    }
+    return (
+        <div className='searchPageContainer'>
+            {
+                restaurants.map((item) => (
+                    // TODO Part I-2: search page front-end
+                    <div className='resBlock' id={item.id} key={item.id}>
+                        <div className='resImgContainer'>
+                            <img className='resImg' src={item.img}>
+                            </img>
+                        </div>
+                        <div className='resInfo'>
+                            <div className='title'>
+                                <p className='name'>{item.name}</p>
+                                <p className='price'>{getPrice(item.price)}</p>
+                                <p className='distance'>{item.distance/1000}km</p>
+                            </div>
+                            <p className='description'>{item.tag.join(', ')}</p>
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
+export default SearchPage
