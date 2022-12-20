b74dc79406b30b6d6ad2ff2ee93a4783a0467643
dlion168 <even.dlion8@gmail.com>
2022-12-20 11:48:45 +0800

    hack3

--- hack3/frontend/src/components/Analytics.js
+++ hack3/frontend/src/components/Analytics.js
@@ -8,9 +8,8 @@ import Category from './Category';
 function Analytics() {
   // TODO 2.2 Use the useQuery hook to get items from backend
   const { data }= useQuery(GET_ITEMS_QUERY);
-  const items = data.items
   // TODO 2.2 End
-
+  const items = data.items
 
   return (
     <div className="grid grid-cols-12 gap-6">
