f20a764a1fc652b8b795323e3d7ff14c3573b83f
dlion168 <even.dlion8@gmail.com>
2022-12-22 16:51:26 +0800

    hw9 copied from hw7, change MONGO_URL to REACT_APP_MONGO_URL

---
+++ hw9/backend/src/models/chatBox.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema
+// Creating a schema, sort of like working with an ORM
+/******* Message Schema *******/
+const MessageSchema = new Schema({
+   sender: { type: String },
+   body: { type: String, required: [true, 'Body field is required.'] },
+  });
+const MessageModel = mongoose.model('Message', MessageSchema);
+  /******* ChatBox Schema *******/
+const ChatBoxSchema = new Schema({
+   name: { type: String, required: [true, 'Name field is required.'] },
+   user: { type: String },
+   messages: [{ type: mongoose.Types.ObjectId, ref: 'Message' }],
+  });
+const ChatBoxModel = mongoose.model('ChatBox', ChatBoxSchema);
+// Creating a table within database with the defined schema
+
+// Exporting table for querying and mutating
+export { MessageModel, ChatBoxModel };
---
+++ hw9/frontend/src/containers/hooks/useChat.js
@@ -0,0 +1,106 @@
+import { useState, createContext, useContext, useEffect } from "react";
+import { message } from 'antd';
+
+const LOCALSTORAGE_KEY = "save-me";
+const savedMe = localStorage.getItem(LOCALSTORAGE_KEY);
+const ChatContext = createContext({
+    status: {},
+    me: "",
+    signedIn: false,
+    messages: [],
+    sendMessage: () => {},
+    //clearMessages: () => {},
+    startChat: ()=>{},
+   });
+
+const client = new WebSocket('ws://127.0.0.1:4000')
+
+const ChatProvider = (props) => {
+    const [status, setStatus] = useState({});
+    const [me, setMe] = useState(savedMe || "")
+    const [signedIn, setSignedIn] = useState(false);
+    const [messages, setMessages] = useState([]);
+
+    client.onmessage = (byteString) => {
+        const { data } = byteString;
+        const [task, payload] = JSON.parse(data);
+        console.log("Responded payload: ", payload)
+        switch (task) {
+            // case "init": {
+            //     setMessages(payload); break;}
+            case "OUTPUT": {
+                console.log("[useChat] Respond received:", payload)
+                setMessages(() =>[...messages, payload]); break; }
+            case "CHATSTART": {
+                setMessages(payload.messages);
+                break;}
+            case "STATUS": {
+                displayStatus(payload);
+                break;}
+            // case "clear": {
+            //     setMessages([]); break;}
+            default: break;
+        }
+    }
+    // const clearMessages = ()=>{
+    //     sendData({
+    //         type: 'CLEAR',
+    //         payload: {}
+    //     });
+    // };
+    const sendMessage = (name, to, body) => {
+        if (!name || !to || !body){
+            throw new Error('Name or to or body required !')
+        }
+        sendData({
+            type: 'MESSAGE',
+            payload: {name, to, body}
+        });
+        console.log("[useChat] Message sent:", name, to, body)
+    };
+    const sendData = async (data) => {
+        await client.send(JSON.stringify(data));
+    };
+    
+    const displayStatus = (s) => {
+        if (s.msg) {
+            const { type, msg } = s;
+            const content = {content: msg, duration: 0.5 };
+            switch (type) {
+                case 'success':
+                    message.success(content);
+                    break;
+                case 'error':
+                default:
+                    message.error(content);
+                    break;
+        }}}
+    
+    const startChat = (name, to)=>{
+        if (!name || !to){
+            throw new Error('Name or to required !')
+        }
+        sendData({
+            type: 'CHAT',
+            payload: {name, to}
+        })
+    }
+    useEffect(() => {
+            if (signedIn) {
+                localStorage.setItem(LOCALSTORAGE_KEY, me);
+            }
+        }, [me, signedIn]);
+
+    return (
+            <ChatContext.Provider
+            value={{
+                status, me, signedIn, messages, setMe, setSignedIn,
+                sendMessage, displayStatus, startChat
+            }}
+            {...props}
+            />
+        );
+};
+
+const useChat = () => useContext(ChatContext);
+export { ChatProvider, useChat };
